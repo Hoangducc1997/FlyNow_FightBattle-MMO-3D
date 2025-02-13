16d3441b7ccbbd8fb57512d0255f3cbe9b181fb5
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-02-13 20:41:52 +0700

    new

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,20 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] InputAction thrust;
-
-    private void OnEnable()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        thrust.Enable();
+        
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void Update()
     {
-        if (thrust.IsPressed())
-        {
-            Debug.Log("Space is pressing");
-        }
+        
     }
 }
