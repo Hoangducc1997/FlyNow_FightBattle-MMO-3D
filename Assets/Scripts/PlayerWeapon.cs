1d9089c41d074c9703ca6f11730842184c43c0a0
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-02-18 18:29:30 +0700

    new

--- Assets/Scripts/PlayerWeapon.cs
+++ Assets/Scripts/PlayerWeapon.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     bool isShooting = false;
-
+    [SerializeField] float speed = 1.0f;
     private void Update()
     {
         ProcessShooting();
