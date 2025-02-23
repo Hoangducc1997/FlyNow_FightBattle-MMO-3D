e1b4ed3a891a4d6db0edb95bd0c99c32b0452552
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-02-23 23:28:49 +0700

    Update/ Add/ FlyingObj

---
+++ Assets/Scripts/FlyingObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FlyingObject : MonoBehaviour
+{
+    [SerializeField] float speedFly = 1.0f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+    //Va chạm với player sẽ làm player mất máu
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position =
+            Vector3.MoveTowards(transform.position, playerPosition, speedFly * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Hit{other.gameObject.name}");
+        //Debug.Log("Hit " + other.name);
+    }
+
+}
