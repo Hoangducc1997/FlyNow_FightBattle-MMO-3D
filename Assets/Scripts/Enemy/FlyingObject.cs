43b2bd93db260d298be4accb340f6f44d602e792
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-02-25 19:11:04 +0700

    Add/Fea/Interact Player And Enemy
    Health

--- Assets/Scripts/Enemy/FlyingObject.cs
+++ Assets/Scripts/Enemy/FlyingObject.cs
@@ -4,11 +4,15 @@ public class FlyingObject : MonoBehaviour
 {
     [SerializeField] float speedFly = 1.0f;
     [SerializeField] Transform player;
+    [SerializeField] int damageAtkPlayer = 1;
+
+    private PlayerInfo playerInfo;
 
     Vector3 playerPosition;
 
     void Start()
     {
+        playerInfo = GetComponent<PlayerInfo>();
         playerPosition = player.transform.position;
     }
 
@@ -21,7 +25,14 @@ public class FlyingObject : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Debug.Log("Chạm vào Terrain (Collision)");
+            Debug.Log("Chạm vào Terrain");
+            Destroy(gameObject, 0.5f);
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Chạm vào Player");
+            playerInfo.TakeDamage(damageAtkPlayer);
             Destroy(gameObject, 0.5f);
         }
     }
--- Assets/Scripts/Player/PlayerInfo.cs
+++ Assets/Scripts/Player/PlayerInfo.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class PlayerInfo : MonoBehaviour
 {
-  
+    [Header("- Health Player -")]
+    [SerializeField] private Slider healthBar;
+    [SerializeField] private int maxHealth = 100;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBar();
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBar != null)
+        {
+            healthBar.value = (float)currentHealth / maxHealth;
+        }
+    }
 }
