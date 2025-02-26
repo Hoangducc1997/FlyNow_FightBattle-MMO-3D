a98b67cadeebf2f038db263d7537b61865640c49
Hoangducc1997 <Hoangducc1997@gmail.com>
2025-02-26 21:10:58 +0700

    Update/Fea/Item

--- Assets/Scripts/UI/Item.cs
+++ Assets/Scripts/UI/Item.cs
@@ -2,22 +2,14 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    [SerializeField] string healthItem;
-    [SerializeField] string passiveItem;
-    [SerializeField] string upgradeLazerItem;
+    [SerializeField] ItemData itemData;
 
-    public void HealthIncrease()
+    private void OnCollisionEnter(Collision collision)
     {
-
+        if(collision.collider.CompareTag("Player"))
+        {
+            itemData.UseItem();
+            Destroy(gameObject);
+        }
     }
-
-    public void PassiveIncrease()
-    {
-
-    }
-    public void UpgradeLazer()
-    {
-
-    }
-
 }
