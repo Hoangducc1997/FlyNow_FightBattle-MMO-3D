7866b394bfa5e2dd1b1856a8bf1ba0cff67db705
ngminhthuan <ng.minh.thuan1710@gmail.com>
2025-03-12 02:12:23 +0700

    Add Start Game and Setting popup, PopupManager, SceneManager, GameManager

---
+++ Assets/Scripts/Game Const/SoundData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class SoundData
+{
+    public float MusicVolume = 0.5f;
+    public float SFXVolume = 0.5f;
+
+    public bool IsMusicMuted = false;
+    public bool IsSFXMuted = false;
+}
